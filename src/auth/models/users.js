4f8023f3139643f7e9d3cdb3d522804196f08ee3
seido <seid783@gmail.com>
2021-02-25 14:51:14 -0800

    seid1

---
+++ src/auth/models/users.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+
+const users = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, required: true, default: 'employee', enum: ['employee', 'manager', 'executive'] },
+});
+
+
+users.virtual('capabilities').get(function () {
+  let acl = {
+    employee: ['read'],
+    manager: ['read', 'create', 'update'],
+    executive: ['read', 'create', 'update', 'delete'],
+  };
+  return acl[this.role];
+});
+
+users.pre('save', async function () {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+});
+
+
+users.statics.authenticateBasic = async function (username, password) {
+  const user = await this.findOne({ username });
+  const valid = await bcrypt.compare(password, user.password);
+  if (valid) { return user; }
+  throw new Error('Invalid User');
+};
+
+
+
+
+module.exports = mongoose.model('users', users);
---
+++ src/server.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// 3rd Party Resources
+const express = require('express');
+// const cors = require('cors');
+const morgan = require('morgan');
+
+// Esoteric Resources
+const errorHandler = require('./error-handlers/500.js');
+const notFound = require('./error-handlers/404.js');
+const authRoutes = require('./auth/router.js');
+const dateRoutes = require('./routes/router.js');
+const logger = require('./middleware/logger.js');
+
+
+// Prepare the express app
+const app = express();
+
+
+// App Level MW
+// app.use(cors());
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(logger);
+
+// Routes
+app.use(authRoutes);
+app.use(dateRoutes);
+
+// Catchalls
+app.use(notFound);
+app.use(errorHandler);
+
+module.exports = {
+  app: app,
+  start: (port) => {
+    app.listen(port, () => {
+      console.log(`Server Up on ==> ${port}`);
+    });
+  },
+};
